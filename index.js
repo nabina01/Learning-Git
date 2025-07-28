2a458a420910769617e705b4daccc4029c7c168a
nabina dahal <dahalnabeena10@gmail.com>
2025-07-28 11:10:50 +0545

    added file2.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-console.log("This is version 1.0.0 of the index.js file");
+console.log("This is version second version of the index.js file");
+
+//if only one file then name of the file 
+//if we need to import multiple files then we can use (git add .)
 
